27aede788bd64673f69325ddc232138ad5bc681b
Rishitha1012 <rishitha1467@gmail.com>
2026-01-03 18:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function getWeather() {
+
+    const city = document.getElementById("cityInput").value;
+    const apiKey = "a1b2c3d4e5f6g7h8";   // <-- paste your key
+    const resultDiv = document.getElementById("weatherResult");
+
+    if (city === "") {
+        resultDiv.innerHTML = "Please enter a city name";
+        return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            if (data.cod === "404") {
+                resultDiv.innerHTML = "City not found ❌";
+            } else {
+                resultDiv.innerHTML = `
+                    <b>City:</b> ${data.name} <br>
+                    <b>Temperature:</b> ${data.main.temp} °C <br>
+                    <b>Weather:</b> ${data.weather[0].description} <br>
+                    <b>Humidity:</b> ${data.main.humidity}% <br>
+                    <b>Wind Speed:</b> ${data.wind.speed} m/s
+                `;
+            }
+        })
+        .catch(error => {
+            resultDiv.innerHTML = "Error fetching data";
+        });
+}
